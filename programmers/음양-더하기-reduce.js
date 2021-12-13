f1f0291cd0e8f993072a6500c47f43f8e778ee6d
Seung Bum <wer06099@naver.com>
2021-12-13 10:38:25 +0900

    baekseungbum: 5일차 코딩테스트

--- programmers/음양-더하기-reduce.js
+++ programmers/음양-더하기-reduce.js
@@ -1,7 +1,5 @@
 function solution(absolutes, signs) {
-    const result = absolutes.reduce((arr, cur, curIndex) => {
+    return absolutes.reduce((arr, cur, curIndex) => {
        return signs[curIndex] ? arr + cur : arr - cur; 
     }, 0);
-    
-    return result;
 };
