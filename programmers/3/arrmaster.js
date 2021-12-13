9f948c169b1a2624bfe68915e8551217c23e1c2a
Lee-Mina <Lee-Mina>
2021-12-13 17:54:27 +0900

    이미나 제출합니다.

--- programmers/3/arrmaster.js
+++
@@ -1,8 +0,0 @@
-// function solution(s) {
-//     if (s.length === 4,6),() true   return false;
-// } console.log(12333)
-
-data="1234"
-if(data.match(/[^0-9]/)){
-    console.log(true);
-    }  else {console.log(false)} 
