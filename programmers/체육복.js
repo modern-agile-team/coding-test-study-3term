925936b6fd981d8d7b0ae51323422463d8a8f988
Seung Bum <wer06099@naver.com>
2021-12-14 11:49:03 +0900

    baekseungbum: 체육복 풀이

---
+++ programmers/체육복.js
@@ -0,0 +1,27 @@
+function solution(n, lost, reserve) {
+    for (let i = 0 ; i < reserve.length; i++) {
+        const hasLost = lost.some((st) => st === reserve[i]);  
+        
+        if (hasLost) {
+            lost = lost.filter((st) => st !== reserve[i]);
+            reserve[i] = 0;
+        };
+    };
+
+    reserve = reserve.filter(st => st).sort();
+    lost = lost.sort();
+    
+    for (let i = 0 ; i < reserve.length; i++) {
+        for (let j = 0 ; j < lost.length; j++) {
+            if (Math.abs(reserve[i] - lost[j]) === 1) {
+                reserve[i] = 0;
+                lost[j] = 0;
+                break;
+            };
+        };
+    };
+    
+    lost = lost.filter(st => st);
+    
+    return n - lost.length;
+}
