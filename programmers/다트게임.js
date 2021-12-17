df56bc06c7223b999fdd0030ed32c9e001835921
Seung Bum <wer06099@naver.com>
2021-12-17 17:34:35 +0900

    baekseungbum: 다트 풀이

--- programmers/다트게임.js
+++ programmers/다트게임.js
@@ -1,31 +1,29 @@
 function solution(dartResult) {
-    let answer = 0;
-    let num = 0;
-    const result = [];
+    const one = /[1-9]?[0]?[SDT][\W]?/g;
+    const two = /[0-9][0]?/g;
+    const arrOne = dartResult.match(one); 
+    const arrTwo = dartResult.match(two);
+    const arrThree = [];
     
-    for (let i = 0; i < dartResult.length; i++) {
-        if (dartResult[i] >= 0 && dartResult[i] <= 9) {
-            if (dartResult[i] === '1' && dartResult[i+1] === '0') {
-                num = 10;
-                i++;
-            } else {
-                num = dartResult[i];
+    for (let i = 0; i < arrOne.length; i++) {
+        if (arrOne[i].includes('S')) {
+            arrThree.push(arrTwo[i]);
+        } else if (arrOne[i].includes('D')) {
+            arrThree.push(Math.pow(arrTwo[i], 2));
+        } else if (arrOne[i].includes('T')) {
+            arrThree.push(Math.pow(arrTwo[i], 3));
+        }; 
+        if (arrOne[i].includes('#')) {
+            arrThree[i] *= -1;
+        } else if (arrOne[i].includes('*')) {
+            if (arrThree.indexOf(arrThree[i]) === 0) {
+                arrThree[i] *= 2;
+            } else if (arrThree.indexOf(arrThree[i]) !== 0) {
+                arrThree[i] *= 2;
+                arrThree[i-1] *= 2;
             };
         };
-         if (dartResult[i] === "S") {
-             result.push(parseInt(num));
-        } else if (dartResult[i] === "D") {
-            result.push(Math.pow(num, 2));
-        } else if (dartResult[i] === "T") {
-            result.push(Math.pow(num, 3));
-        } else if (dartResult[i] === "#") {
-            result[result.length - 1] *= -1;
-        } else if (dartResult[i] === "*") {
-            result[result.length - 1] *= 2;
-            if (result[result.length - 2]) {
-                result[result.length - 2] *= 2;   
-            };   
-        };
-    };
-    return result.reduce((acc, crr) => acc + crr, 0);
+    }; 
+    
+    return eval(arrThree.join("+"));
 }
