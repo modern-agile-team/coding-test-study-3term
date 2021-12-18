2b9e17654d30588b8687ade9f59ad2677636dfb6
HanGyeol Lee <69745441+leephoter@users.noreply.github.com>
2021-12-18 23:50:42 +0900

    Update [1차]-다트-게임.js

--- [1차]-다트-게임.js
+++ [1차]-다트-게임.js
@@ -9,11 +9,11 @@ function solution(dartResult) {
         .replace(/ {1,}\*/g, "*")
         .replace(/ {1,}$/g, "");
     let newArray = newResult.split(" ");
-    newArray = newArray.forEach((item, index) => {
-      if (item.match(/\*{1,}/g).length > 1 && index > 0) {
-          newArray[index - 1] = newArray[index - 1] + '*2'
+    newArray.forEach((item, index) => {
+        if (item.match(/\*{1,}/g).length > 1 && index > 0) {
+            newArray[index - 1] = newArray[index - 1] + "*2";
         }
     });
-    const result = newArray.join(' ').replace(/ /g, '+').replace(/\(/g, '*(');
-    return (eval(result));
+    const result = newArray.join(" ").replace(/ /g, "+").replace(/\(/g, "*(")
+    return eval(result);
 }
