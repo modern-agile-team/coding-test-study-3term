161633b2c6fd70e44f9ac83a152108e437df6559
leephoter <estarg51@gmail.com>
2022-01-09 20:42:00 +0900

    이한결 제출합니다 (3기 알고리즘 1차)

---
+++ [1차]-비밀지도.js
@@ -0,0 +1,24 @@
+function solution(n, arr1, arr2) {
+    const LEN_MAP = n;
+
+    const firstArray = arr1.map((item) => item.toString(2));
+    const secondArray = arr2.map((item) => item.toString(2));
+
+    let mergedMap = firstArray.map((item, index) =>
+        String(Number(item) + Number(secondArray[index]))
+    );
+
+    mergedMap = mergedMap.map((item) => {
+        while (item.length < LEN_MAP) {
+            item = `0${item}`;
+        }
+        return item;
+    });
+
+    mergedMap = mergedMap.map((item) =>
+        item.split("").map((each) => (each !== "0" ? "#" : " "))
+    );
+    mergedMap = mergedMap.map((item) => item.join(""));
+
+    return mergedMap;
+}
