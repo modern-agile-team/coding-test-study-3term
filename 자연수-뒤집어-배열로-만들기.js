7b8eef3cd11f1a8c27663413352e129759b1a7cd
leephoter <estarg51@gmail.com>
2022-01-14 11:06:34 +0900

    git 에러

--- 자연수-뒤집어-배열로-만들기.js
+++
@@ -1,7 +0,0 @@
-function solution(n) {
-    const result = [];
-    for (let i = 0; i < `${n}`.length; i++) {
-        result.push(parseInt((n % 10 ** (i + 1)) / 10 ** i));
-    }
-    return result;
-}
